16cdf75899035f82e28d34e1d40e385f77806942
Janitha Gamage <j.sarwapriya@gmail.com>
2023-04-14 19:55:37 +0530

    Chnage API Url

--- Angular/my-app/src/app/upload-file/upload-file.component.ts
+++ Angular/my-app/src/app/upload-file/upload-file.component.ts
@@ -20,7 +20,7 @@ export class UploadFileComponent {
     
     console.log("onUpload " +this.selectedFile);
     if (this.selectedFile) {
-      const url = `${this.baseUrl}api/accountBalance/add-account-balance`;
+      const url = `${this.baseUrl}/api/accountBalance/add-account-balance`;
       //this.http..post()
       this.fileService.uploadFile(url,this.selectedFile).subscribe(
         response => {
