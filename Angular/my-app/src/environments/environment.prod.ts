d9301d2cd633af4701313da9c1d6db188b7b3047
Janitha Gamage <j.sarwapriya@gmail.com>
2023-04-21 19:42:20 +0530

    Add Github deployment to Angular app

--- Angular/my-app/src/environments/environment.prod.ts
+++ Angular/my-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    //baseUrl: "https://jondellaccbalance.azurewebsites.net/"
-    baseUrl: "http://localhost:7106"
+    baseUrl: "https://accountbalanceviewer.azurewebsites.net/"
+    //baseUrl: "http://localhost:7106"
 }
